bb7ee11d5fceb4c8fc371e4d50d111fdc7017825
geraldstanje <Gerald.Stanje@gmail.com>
2014-02-05 11:56:04 -0800

    code formating

--- encryptUtil/main.cpp
+++ encryptUtil/main.cpp
@@ -9,19 +9,19 @@ int main(int argc, char *argv[])
   if (allArgs.size() != 5) {
     // argv[0] is the program name
     std::cerr << "usage: "<< allArgs[0] << " [-n #] [-k keyfile]\n";
-	  return -1;
+    return -1;
   }
     
   EncryptUtil crypto;
   if(!crypto.read_key_from_file(allArgs[4])) {
-  	return -1;
+    return -1;
   }
   if(!crypto.set_number_of_threads(allArgs[2])) {
-  	return -1;
+    return -1;
   }
   crypto.set_stream_buffer_size(10000);
   if(!crypto.input_stream_encrypt()) {
-  	return -1;
+    return -1;
   }
   
   return 0;
