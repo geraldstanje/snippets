75200eae26fd99dff8d22fa9435ec411714b0950
geraldstanje <Gerald.Stanje@gmail.com>
2014-02-16 16:23:55 -0800

    formatted code

--- cardDeck/rounds.c
+++ cardDeck/rounds.c
@@ -1,206 +1,268 @@
+#include "rounds.h"
 #include <stdio.h>
 #include <math.h>
 #include <stdbool.h>
 #include <stdlib.h>
 
 /*
- * creates a new card on the heap
+ * creates a new card node
  */
-static card_t *new_card(int number) {
-	card_t *node = (card_t*)malloc(sizeof(card_t));
-	node->number = number;
-	node->next = NULL;
-	return node;
+static card_t *new_card(unsigned long number) {
+    card_t *node = (card_t*)malloc(sizeof(card_t));
+    
+    // error checking
+    if(node != NULL) {
+        node->number = number;
+        node->next = NULL;
+    }
+    
+    return node;
 }
 
 /*
  * allocates an empty list  
  */
 static list_t *create_empty_list() {
-	list_t *l = (list_t*)malloc(sizeof(list_t));
-	l->front = NULL;
-	l->back = NULL;
+    list_t *l = (list_t*)malloc(sizeof(list_t));
+    l->front = NULL;
+    l->back = NULL;
 
-	return l;
+    return l;
 }
 
 /*
  * deallocates the entire list
  */
 static void delete_list(list_t **l) {
-	card_t *curr = (*l)->front;
+    card_t *curr = (*l)->front;
 
-	while (curr) {
-		card_t *to_del = curr;
-		curr = curr->next;
-		free(to_del);
-	}
+    while (curr) {
+        card_t *to_del = curr;
+        curr = curr->next;
+        free(to_del);
+    }
 
-	free(*l);
-	*l = NULL;
+    free(*l);
+    *l = NULL;
 }
 
 /*
  * removes the first card from the list and returns it
  */
 static card_t *remove_front(list_t *l) {
-	card_t *front = l->front;
+    card_t *front = l->front;
 
-	if (l->front) {
-		l->front = l->front->next;
-	}
+    if (l->front) {
+        l->front = l->front->next;
+    }
 
-	return front;
+    return front;
 }
 
 /*
  * inserts a new card at the beginning of the list
  */
 static void insert_front(list_t *l, card_t *c) {
-	c->next = NULL;
+    c->next = NULL;
 
-	if (l->front == NULL) {
-		l->front = c;
-		l->back = c;
-	}
-	else {
-		c->next = l->front;
-		l->front = c;
-	}
+    if (l->front == NULL) {
+        l->front = c;
+        l->back = c;
+    }
+    else {
+        c->next = l->front;
+        l->front = c;
+    }
 }
 
 /*
  * inserts a new card at the end of the list
  */
 static void insert_back(list_t *l, card_t *c) {
-	c->next = NULL;
+    c->next = NULL;
 
-	if (l->front == NULL) {
-		l->front = c;
-		l->back = c;
-	}
-	else {
-		l->back->next = c;
-		l->back = c;
-	}
+    if (l->front == NULL) {
+        l->front = c;
+        l->back = c;
+    }
+    else {
+        l->back->next = c;
+        l->back = c;
+    }
 }
 
 /*
  * inserts a new card at the end of the list
  */
-static void add_new_card(list_t *l, unsigned int number) {
-	card_t *c = new_card(number);
-
-	if (l->front == NULL) {
-		l->front = c;
-		l->back = c;
-	}
-	else {
-		l->back->next = c;
-		l->back = c;
-	}
+static bool add_new_card(list_t *l, unsigned int number) {
+    card_t *c = new_card(number);
+    
+    // error checking
+    if(c == NULL) {
+        return false;
+    }
+    
+    if (l->front == NULL) {
+        l->front = c;
+        l->back = c;
+    }
+    else {
+        l->back->next = c;
+        l->back = c;
+    }
+    
+    return true;
 }
 
 /*
  * prints the complete list of cards
  */
 static void print(list_t *l) {
-	card_t *curr = l->front;
+    card_t *curr = l->front;
 
-	while (curr != NULL) {
-		printf("%d ", curr->number);
-		curr = curr->next;
-	}
-	printf("\n");
+    printf("hand: ");
+    while (curr != NULL) {
+        printf("%lu ", curr->number);
+        curr = curr->next;
+    }
+    printf("\n");
 }
 
 /*
  * prints the complete list of cards
  */
-static void init_cards(list_t *hand, unsigned int number_of_cards) {
-	unsigned int i = 0;
-
-	for (i = 0; i < number_of_cards; i++) {
-		add_new_card(hand, i);
-	}
+static bool init_cards(list_t *hand, unsigned long number_of_cards) {
+    unsigned long i = 0;
+    bool is_valid = true;
+    
+    for (i = 0; i < number_of_cards; i++) {
+        is_valid = add_new_card(hand, i);
+        
+		if(!is_valid) {
+            return false;
+        }
+    }
+	
+    return true;
 }
 
 /*
  * checks if the list is sorted by increasing numbers
  */
 static bool is_sorted(list_t *hand, int number_of_cards) {
-	unsigned int curr_number = 0;
-	card_t *curr = hand->front;
+    unsigned long curr_number = 0;
+    card_t *curr = hand->front;
 
-	for (curr_number = 0; curr_number < number_of_cards; curr_number++) {
-		if (curr->number != curr_number) {
-			return false;
-		}
+    for (curr_number = 0; curr_number < number_of_cards; curr_number++) {
+        if (curr->number != curr_number) {
+            return false;
+        }
 
-		curr = curr->next;
-	}
+        curr = curr->next;
+    }
 
-	return true;
+    return true;
 }
 
 /*
  * runs one round of the card shuffling algorithm
  */
 static void next_round(list_t *hand, list_t *table) {
-	while (hand->front) {
-		card_t *top_card = NULL;
+    card_t *top_card = NULL;
+	
+    while (hand->front) { // check if cards are available
+        top_card = NULL;
 
-		// Take the top card off the hand and set it on the table
-		top_card = remove_front(hand);
-		insert_front(table, top_card);
+        // Take the top card off the hand and set it on the table
+        top_card = remove_front(hand);
+        insert_front(table, top_card);
+        // no cards on table
+        if (!top_card) {
+            break;
+        }
 
-		if (!top_card) {
-			break;
-		}
-
-		// Take the next card off the top and put it on the bottom of the hand in your hand
-		top_card = remove_front(hand);
-		if (!top_card) {
-			break;
-		}
-		insert_back(hand, top_card);
-	}
+        // Take the next card off the top and put it on the bottom of the hand in your hand
+        top_card = remove_front(hand);
+        // no cards in hand
+        if (!top_card) {
+            break;
+        }
+        insert_back(hand, top_card);
+    }
 }
 
 /*
- * picks up the new hand from the table
+ * picks up all cards from the card from the table and assigns it to hand
  */
 static void pickup_hand(list_t **hand, list_t **table) {
-	list_t *tmp = *hand;
-	*hand = *table;
-	*table = tmp;
+    list_t *tmp = *hand;
+    *hand = *table;
+    *table = tmp;
 }
 
+/*
+ * allocated memory for hand and table and initializes the hand
+ */
+static bool init_lists(list_t **hand, list_t **table, unsigned long number_of_cards) {
+    // allocate memory for hand
+    *hand = create_empty_list();
+    if(*hand == NULL) {
+        return false; // return false if memory allocation for hand fails
+    }
+	
+    // allocate memory for table
+    *table = create_empty_list();
+    if(*table == NULL) {
+        delete_list(hand); // cleanup already allocated memory from before
+        return false; // return false if memory allocation for table fails
+    }
+	
+    // initialize cards for hand
+	bool is_valid = init_cards(*hand, number_of_cards);
+    if(!is_valid) {
+        delete_list(hand); // cleanup already allocated memory from before
+        delete_list(table); // cleanup already allocated memory from before
+        return false;
+    }
+    
+    return true;
+}
+    
 /*
  * determines how many rounds it will take to put a deck back into the original order
  */
-unsigned int get_number_of_rounds(unsigned int number_of_cards) {
-	if (number_of_cards <= 2) {
-		return number_of_cards;
-	}
+bool get_number_of_rounds(unsigned long number_of_cards, unsigned long *rounds) {
+    if (number_of_cards <= 2) {
+        *rounds = number_of_cards;
+        return true;
+    }
 
-	list_t *hand = create_empty_list();
-	list_t *table = create_empty_list();
-	bool is_equal = false;
-	unsigned int rounds = 0;
+    // define hand and table (both are a list of cards)
+    list_t *hand = NULL;
+    list_t *table = NULL;
+    
+    // allocate memory for hand and table
+    // initialize hand
+    if(!init_lists(&hand, &table, number_of_cards)) {
+        return false;
+    }
+    
+    bool is_equal = false;
+    
+    do {
+        (*rounds)++;
+        next_round(hand, table);
+        pickup_hand(&hand, &table);
 
-	init_cards(hand, number_of_cards);
+#ifdef DEBUG_INFOS
+        print(hand);
+#endif
 
-	do {
-		rounds++;
-		next_round(hand, table);
-		pickup_hand(&hand, &table);
-		//print(hand);
-		is_equal = is_sorted(hand, number_of_cards);
-	} while (!is_equal);
+        is_equal = is_sorted(hand, number_of_cards);
+    } while (!is_equal);
 
-	delete_list(&hand);
-	delete_list(&table);
+    delete_list(&hand);
+    delete_list(&table);
 
-	return rounds;
+    return true;
 }
