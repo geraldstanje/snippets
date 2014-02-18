47b4b532d6495e0a1b571911a0169980a6298fdf
geraldstanje <Gerald.Stanje@gmail.com>
2014-02-17 16:39:25 -0800

    formatted code

--- fb_login_m.go
+++ fb_login_m.go
@@ -13,9 +13,9 @@ import (
     "strings"
     "io/ioutil"
     "encoding/json"
-	"net/http/cookiejar"
-	"html"
-	"flag"
+    "net/http/cookiejar"
+    "html"
+    "flag"
 )
 
 func get_data(s string, start_str string, end_str string) (string, error) {
@@ -49,7 +49,7 @@ func write_to_file(file_str string, s string) error {
 }
 
 type FBEngine struct {
-	client *http.Client
+    client *http.Client
 }
 
 func NewFBEngine() *FBEngine {
@@ -81,14 +81,14 @@ func generate_post_data(email string, m_ts string, li string, lsd string, charse
 }
 
 func fb_login(fbengine *FBEngine, email string, pass string) (string, string, string, error) {
-	const use_new_http_request = 0
+    const use_new_http_request = 0
 
-	///////////////////
+    ///////////////////
     fmt.Println("\nSend First request")
     resp, err := fbengine.client.Get("http://m.facebook.com/")
-	defer resp.Body.Close()
+    defer resp.Body.Close()
     if err != nil {
-		return "", "", "", fmt.Errorf("Get request failed: %s", err)
+	return "", "", "", fmt.Errorf("Get request failed: %s", err)
     }
 
     // should be: redirect_url := resp.Request.URL.String()
@@ -97,12 +97,12 @@ func fb_login(fbengine *FBEngine, email string, pass string) (string, string, st
     // Write to file
     b, err := ioutil.ReadAll(resp.Body)
     if err != nil {
-		return "", "", "", fmt.Errorf("Read HTML body failed: %s", err)
+	return "", "", "", fmt.Errorf("Read HTML body failed: %s", err)
     }
     s := string(b)
     err = write_to_file("output1.html", s)
     if err != nil {
-		return "", "", "", fmt.Errorf("Write file failed: %s", err)
+	return "", "", "", fmt.Errorf("Write file failed: %s", err)
     }      
 
     // Print cookies
@@ -116,7 +116,7 @@ func fb_login(fbengine *FBEngine, email string, pass string) (string, string, st
     charset_test, _ := get_data(s, "name=\"charset_test\" value=\"", "\"")
     signup_layout, _ := get_data(s, "name=\"signup_layout\" value=\"", "\"")
 
-	///////////////////
+    ///////////////////
     fmt.Println("\nSend second request")
     if use_new_http_request == 0 {
 		resp, err = fbengine.client.PostForm(redirect_url,
@@ -133,7 +133,7 @@ func fb_login(fbengine *FBEngine, email string, pass string) (string, string, st
 				   "version": {"1"},
 				   "signup_layout": {signup_layout},
 				   "pass": {pass}})
-	} else if use_new_http_request == 1 {
+    } else if use_new_http_request == 1 {
 		// get post data
 		data := generate_post_data(email, m_ts, li, lsd, charset_test, signup_layout, pass)
 		
@@ -156,12 +156,12 @@ func fb_login(fbengine *FBEngine, email string, pass string) (string, string, st
 		req.Header.Set("Accept-Language", "en-gb,en;q=0.5")
 
 		resp, err = fbengine.client.Do(req)
-	}
+    }
 
     if err != nil {
-		return "", "", "", fmt.Errorf("Post request failed: %s", err)
+	return "", "", "", fmt.Errorf("Post request failed: %s", err)
     }
-	defer resp.Body.Close()
+    defer resp.Body.Close()
 
     // should be: redirect_url := resp.Request.URL.String()
     redirect_url, _ = url.QueryUnescape(resp.Request.URL.String())
@@ -169,105 +169,105 @@ func fb_login(fbengine *FBEngine, email string, pass string) (string, string, st
     // Write to file
     b, err = ioutil.ReadAll(resp.Body)
     if err != nil {
-		return "", "", "", fmt.Errorf("Read HTML body failed: %s", err)
+	return "", "", "", fmt.Errorf("Read HTML body failed: %s", err)
     }
     s = string(b)
     err = write_to_file("output2.html", s)
     if err != nil {
-		return "", "", "", fmt.Errorf("Write file failed: %s", err)
-    }
-
-	// print cookies
-    for _, c := range resp.Cookies() { fmt.Println(c) }
-
-	// extract some data for the post request
-	status_update_url, _ := get_data(s, "<form method=\"post\" class=\"composer_form\" id=\"composer_form\" action=\"", "\"")
-    status_update_url = "https://m.facebook.com" + status_update_url  
-    fb_dtsg, _ := get_data(s, "name=\"fb_dtsg\" value=\"", "\"")
-    privacy, _ := get_data(s, "name=\"privacy\" value=\"", ";\"")
-	privacy = html.UnescapeString(privacy)
-
-	return status_update_url, fb_dtsg, privacy, nil
-}
-
-func fb_post_comment(fbengine *FBEngine, comment []string, status_update_url string, fb_dtsg string, privacy string) (string, error) {
-	var text_to_share string
-	for _, c := range comment { text_to_share += c + " "}
-
-	fmt.Println("\nSend third request")
-	resp, err := fbengine.client.PostForm(status_update_url,
-    url.Values{"fb_dtsg": {fb_dtsg},
-               "update": {"Share"},
-               "target": {""}, 
-               "status": {text_to_share},
-			   "privacy": {privacy}})
-    if err != nil {
-		return "", fmt.Errorf("Post request failed: %s", err)
-    }
-	defer resp.Body.Close()
-
-    // Write to file
-    b, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-		return "", fmt.Errorf("Post request failed: %s", err)
-    }
-    s := string(b)
-    err = write_to_file("output3.html", s)
-    if err != nil {
-		return "", fmt.Errorf("Post request failed: %s", err)
-    }
-
-	// print cookies
-    for _, c := range resp.Cookies() { fmt.Println(c) }
-
-	// extract some data for the get request
-	logout_url, _ := get_data(s, "<a href=\"/logout.php?", "\"")
-	logout_url = "https://m.facebook.com/logout.php?" + logout_url
-
-	return logout_url, nil
-}
-
-func fb_logout(fbengine *FBEngine, logout_url string) (error) {
-	///////////////////
-	fmt.Println("\nSend fourth request")
-	resp, err := fbengine.client.Get(logout_url)
-    if err != nil {
-		return fmt.Errorf("Get request failed: %s", err)
-    }
-	defer resp.Body.Close()
-
-    // Write to file
-    b, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-		return fmt.Errorf("Read HTML body failed: %s", err)
-    }
-    s := string(b)
-    err = write_to_file("output4.html", s)
-    if err != nil {
-		return fmt.Errorf("Write file failed: %s", err)
+	return "", "", "", fmt.Errorf("Write file failed: %s", err)
     }
 
     // print cookies
     for _, c := range resp.Cookies() { fmt.Println(c) }
 
-	return nil
+    // extract some data for the post request
+    status_update_url, _ := get_data(s, "<form method=\"post\" class=\"composer_form\" id=\"composer_form\" action=\"", "\"")
+    status_update_url = "https://m.facebook.com" + status_update_url  
+    fb_dtsg, _ := get_data(s, "name=\"fb_dtsg\" value=\"", "\"")
+    privacy, _ := get_data(s, "name=\"privacy\" value=\"", ";\"")
+    privacy = html.UnescapeString(privacy)
+
+    return status_update_url, fb_dtsg, privacy, nil
+}
+
+func fb_post_comment(fbengine *FBEngine, comment []string, status_update_url string, fb_dtsg string, privacy string) (string, error) {
+    var text_to_share string
+    for _, c := range comment { text_to_share += c + " "}
+
+    fmt.Println("\nSend third request")
+    resp, err := fbengine.client.PostForm(status_update_url,
+    url.Values{"fb_dtsg": {fb_dtsg},
+               "update": {"Share"},
+               "target": {""}, 
+               "status": {text_to_share},
+	       "privacy": {privacy}})
+    if err != nil {
+	return "", fmt.Errorf("Post request failed: %s", err)
+    }
+    defer resp.Body.Close()
+
+    // Write to file
+    b, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+	return "", fmt.Errorf("Post request failed: %s", err)
+    }
+    s := string(b)
+    err = write_to_file("output3.html", s)
+    if err != nil {
+	return "", fmt.Errorf("Post request failed: %s", err)
+    }
+
+    // print cookies
+    for _, c := range resp.Cookies() { fmt.Println(c) }
+
+    // extract some data for the get request
+    logout_url, _ := get_data(s, "<a href=\"/logout.php?", "\"")
+    logout_url = "https://m.facebook.com/logout.php?" + logout_url
+
+    return logout_url, nil
+}
+
+func fb_logout(fbengine *FBEngine, logout_url string) (error) {
+    ///////////////////
+    fmt.Println("\nSend fourth request")
+    resp, err := fbengine.client.Get(logout_url)
+    if err != nil {
+	return fmt.Errorf("Get request failed: %s", err)
+    }
+    defer resp.Body.Close()
+
+    // Write to file
+    b, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+	return fmt.Errorf("Read HTML body failed: %s", err)
+    }
+    s := string(b)
+    err = write_to_file("output4.html", s)
+    if err != nil {
+	return fmt.Errorf("Write file failed: %s", err)
+    }
+
+    // print cookies
+    for _, c := range resp.Cookies() { fmt.Println(c) }
+
+    return nil
 }
 
 func main() {
-	flag.Parse()
+    flag.Parse()
     args := flag.Args()
     if len(args) < 3 {
         fmt.Println("Please pass arguments: Email, Password, Comment. e.g. fb_login_m.exe test@gmail.com password text to post")
-		return
+	return
     }
 
-	// here the login infos
-	var email = args[0]
-	var pass = args[1]
-	var text = args[2:]
+    // here the login infos
+    var email = args[0]
+    var pass = args[1]
+    var text = args[2:]
 
-	var fbengine FBEngine = *NewFBEngine()
-	status_update_url, fb_dtsg, privacy, _ := fb_login(&fbengine, email, pass)
-	logout_url, _ := fb_post_comment(&fbengine, text, status_update_url, fb_dtsg, privacy)
-	fb_logout(&fbengine, logout_url)
+    var fbengine FBEngine = *NewFBEngine()
+    status_update_url, fb_dtsg, privacy, _ := fb_login(&fbengine, email, pass)
+    logout_url, _ := fb_post_comment(&fbengine, text, status_update_url, fb_dtsg, privacy)
+    fb_logout(&fbengine, logout_url)
 }		
