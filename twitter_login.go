6a5c5bc4c1ece60094113e60bf66cdc2521daa88
geraldstanje <Gerald.Stanje@gmail.com>
2014-03-18 13:15:35 -0700

    formatted code

--- twitter_login.go
+++ twitter_login.go
@@ -1,8 +1,8 @@
 package main
 
 /*
-This snippet is written in GoLang and posts a comment on your twitter timeline. Includes: login, post comment, logout
-fb_login_m.exe test@gmail.com password this text will posted
+This snippet is written in GoLang and posts a comment on your Twitter timeline. Includes: login, post comment, logout
+./twitter test@gmail.com password this text will be tweeted
 */
 
 import (
@@ -12,7 +12,7 @@ import (
     "os"
     "strings"
     "io/ioutil"
-    "encoding/json"
+    //"encoding/json"
     "net/http/cookiejar"
     //"html"
     "net/http/httputil"
@@ -50,82 +50,21 @@ func write_to_file(file_str string, s string) error {
     return err
 }
 
-type FBEngine struct {
+type TwitterEngine struct {
     client *http.Client
 }
 
-func NewFBEngine() *FBEngine {
+func NewTwitterEngine() *TwitterEngine {
     jar, _ := cookiejar.New(nil)
-    return &FBEngine{client: &http.Client{nil, nil, jar}}
+    return &TwitterEngine{client: &http.Client{nil, nil, jar}}
 }
 
-func generate_post_data(email string, pass string, authenticity_token string) string {
-    data, err := json.Marshal(map[string]interface{} {
-        "session[username_or_email]": email,
-        "session[password]": pass,
-        "remember_me": "1",
-        "return_to_ssl": "true",
-        "scribe_log": "",
-        "redirect_after_login": "/",
-        "authenticity_token": authenticity_token,
-        /*"username": email,
-        "password:": pass,
-        "commit": "Sign in",
-        "authenticity_token": authenticity_token,
-        */
-    })
-    if err != nil {
-        fmt.Println(err)
-    }
- 
-    return string(data)
-}
-
-func twitter_login(fbengine *FBEngine, email string, pass string) (string, error) {
-    const use_new_http_request = 1
-
-    ///////////////////
-    /*fmt.Println("\nSend First request")
-    resp, err := fbengine.client.Get("https://twitter.com/login") //https://twitter.com")
-    defer resp.Body.Close()
-    if err != nil {
-        return "", "", "", fmt.Errorf("Get request failed: %s", err)
-    }
-
-    // should be: redirect_url := resp.Request.URL.String()
-    redirect_url, _ := url.QueryUnescape(resp.Request.URL.String())
-
-    // Write to file
-    b, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-        return "", "", "", fmt.Errorf("Read HTML body failed: %s", err)
-    }
-    s := string(b)
-    err = write_to_file("output1.html", s)
-    if err != nil {
-        return "", "", "", fmt.Errorf("Write file failed: %s", err)
-    }
-
-    // Print cookies
-    for _, c := range resp.Cookies() { fmt.Println(c) }
-   
-    // extract some data for the post request
-    //redirect_url = "https://twitter.com/sessions"
-    redirect_url, _ = get_data(s, "<form action=\"https", "\"")
-    redirect_url = "https" + redirect_url
-    //authenticity_token, _ := get_data(s, "name=\"authenticity_token\" type=\"hidden\" value=\"", "\"")
-    authenticity_token, _ := get_data(s, "name=\"authenticity_token\" value=\"", "\">")
-
-    fmt.Println(email)
-    fmt.Println(pass)
-    fmt.Println(redirect_url)
-    fmt.Println(authenticity_token)
-    */
+func twitter_login(TwitterEngine *TwitterEngine, email string, pass string) (string, error) {
     req, err := http.NewRequest("GET", "https://twitter.com/login", nil)
     if err != nil {
         return "", fmt.Errorf("Post request failed: %s", err)
     }
-    resp, err := fbengine.client.Do(req)
+    resp, err := TwitterEngine.client.Do(req)
     
     if err != nil {
         return "", fmt.Errorf("Post request failed: %s", err)
@@ -159,68 +98,40 @@ func twitter_login(fbengine *FBEngine, email string, pass string) (string, error
     
     ///////////////////
     fmt.Println("\nSend second request")
-    if use_new_http_request == 0 {
-        resp, err = fbengine.client.PostForm(redirect_url,
-        url.Values{"session[username_or_email]": {email},
-                   "session[password]": {pass},
-                   "remember_me": {"1"},
-                   "return_to_ssl": {"true"},
-                   "scribe_log": {""},
-                   "redirect_after_login": {"/"},
-                   "authenticity_token": {authenticity_token}})
-        /*url.Values{"username": {email},
-                   "password:": {pass},
-                   "commit": {"Sign in"},
-                   "authenticity_token": {authenticity_token}})
-                   */
-    } else if use_new_http_request == 1 {
-        // get post data
-        //data := generate_post_data(email, pass, authenticity_token)
-
-        data := url.Values{"session[username_or_email]": {email},
-                   "session[password]": {pass},
-                   "remember_me": {"1"},
-                   "return_to_ssl": {"true"},
-                   "scribe_log": {""},
-                   "redirect_after_login": {"/"},
-                   "authenticity_token": {authenticity_token}}
+    
+    data := url.Values{"session[username_or_email]": {email},
+                       "session[password]": {pass},
+                       "remember_me": {"1"},
+                       "return_to_ssl": {"true"},
+                       "scribe_log": {""},
+                       "redirect_after_login": {"/"},
+                       "authenticity_token": {authenticity_token}}
                    
-        // prepare post request
-        //bytes.NewBufferString(data.Encode()))
-        req, err := http.NewRequest("POST", redirect_url, strings.NewReader(data.Encode())); //strings.NewReader(data()))
-        if err != nil {
-            return "", fmt.Errorf("Post request failed: %s", err)
-        }
-
-        // add cookies to the request which are in jar cookie
-        //for _, cookie := range fbengine.client.Cookies() { //req.URL) {
-        //    fmt.Println(cookie)
-        //req.AddCookie(cookie)
-        //}
-
-        req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
-        req.Header.Set("Accept-Encoding", "gzip,deflate,sdch")
-        req.Header.Set("Accept-Language", "en-US,en;q=0.8")
-        req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-        //req.Header.Set("Connection", "Connection:keep-alive")
-        //req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-        //req.Header.Set("Referer", "https://twitter.com/")
-        req.Header.Set("Host", "twitter.com")
-        req.Header.Set("User-Agent", "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:16.0) Gecko/20100101 Firefox/16.0")
-        req.Header.Set("Origin", "https://twitter.com")
-        req.Header.Set("Referer", "https://twitter.com/")
-        req.Header.Set("Cache-Control", "max-age=0")
-        //req.Header.Set("Content-Length", strconv.Itoa(len(data)))
-        //req.SetBasicAuth("u", "p")
-        
-        //dumpHead, _ := httputil.DumpRequest(req, false)
-        dumpBody, _ := httputil.DumpRequest(req, true)
-
-        //fmt.Println(string(dumpHead))
-        fmt.Println(string(dumpBody))
-        
-        resp, err = fbengine.client.Do(req)
+    // prepare post request
+    req, err = http.NewRequest("POST", redirect_url, strings.NewReader(data.Encode()));
+    if err != nil {
+       return "", fmt.Errorf("Post request failed: %s", err)
     }
+
+    req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+    req.Header.Set("Accept-Encoding", "gzip,deflate,sdch")
+    req.Header.Set("Accept-Language", "en-US,en;q=0.8")
+    req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+    req.Header.Set("Host", "twitter.com")
+    req.Header.Set("User-Agent", "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:16.0) Gecko/20100101 Firefox/16.0")
+    req.Header.Set("Origin", "https://twitter.com")
+    req.Header.Set("Referer", "https://twitter.com/")
+    req.Header.Set("Cache-Control", "max-age=0")
+    //req.Header.Set("Content-Length", strconv.Itoa(len(data)))
+    //req.SetBasicAuth("u", "p")
+        
+    //dumpHead, _ := httputil.DumpRequest(req, false)
+    dumpBody, _ := httputil.DumpRequest(req, true)
+
+    //fmt.Println(string(dumpHead))
+    fmt.Println(string(dumpBody))
+       
+    resp, err = TwitterEngine.client.Do(req)
        
     if err != nil {
         return "", fmt.Errorf("Post request failed: %s", err)
@@ -248,19 +159,10 @@ func twitter_login(fbengine *FBEngine, email string, pass string) (string, error
 
     fmt.Println(resp.Header)
     
-    // extract some data for the post request
-    /*status_update_url, _ := get_data(s, "<form method=\"post\" class=\"composer_form\" id=\"composer_form\" action=\"", "\"")
-    status_update_url = "https://m.facebook.com" + status_update_url
-    fb_dtsg, _ := get_data(s, "name=\"fb_dtsg\" value=\"", "\"")
-    privacy, _ := get_data(s, "name=\"privacy\" value=\"", ";\"")
-    privacy = html.UnescapeString(privacy)
-    */
-    
-    return authenticity_token, fmt.Errorf("err")
+    return authenticity_token, nil
 }
 
-func twitter_geo_locate(fbengine *FBEngine, authenticity_token string) (error) {
-      
+func twitter_geo_locate(TwitterEngine *TwitterEngine, authenticity_token string) (error) {
     data := url.Values{"authenticity_token": {authenticity_token}}
                    
     // prepare post request
@@ -289,7 +191,7 @@ func twitter_geo_locate(fbengine *FBEngine, authenticity_token string) (error) {
     //fmt.Println(string(dumpHead))
     fmt.Println(string(dumpBody))
         
-    resp, err := fbengine.client.Do(req)
+    resp, err := TwitterEngine.client.Do(req)
         
     if err != nil {
         return fmt.Errorf("Post request failed: %s", err)
@@ -320,7 +222,7 @@ func twitter_geo_locate(fbengine *FBEngine, authenticity_token string) (error) {
     return nil
 }
 
-func twitter_post_comment(fbengine *FBEngine, authenticity_token string, comment []string) (string, error) {   
+func twitter_post_comment(TwitterEngine *TwitterEngine, authenticity_token string, comment []string) (string, error) {   
     var tweet string
     for _, c := range comment { tweet += c + " "}
     
@@ -356,7 +258,7 @@ func twitter_post_comment(fbengine *FBEngine, authenticity_token string, comment
     //fmt.Println(string(dumpHead))
     fmt.Println(string(dumpBody))
         
-    resp, err := fbengine.client.Do(req)
+    resp, err := TwitterEngine.client.Do(req)
         
     if err != nil {
         return "", fmt.Errorf("Post request failed: %s", err)
@@ -391,7 +293,7 @@ func twitter_post_comment(fbengine *FBEngine, authenticity_token string, comment
     return logout_url, nil
 }
 
-func twitter_logout(fbengine *FBEngine, authenticity_token string, logout_url string) (error) {     
+func twitter_logout(TwitterEngine *TwitterEngine, authenticity_token string, logout_url string) (error) {     
     data := url.Values{"reliability_event": {},
                        "scribe_log": {},
                        "authenticity_token": {authenticity_token}}
@@ -420,7 +322,7 @@ func twitter_logout(fbengine *FBEngine, authenticity_token string, logout_url st
     //fmt.Println(string(dumpHead))
     fmt.Println(string(dumpBody))
         
-    resp, err := fbengine.client.Do(req)
+    resp, err := TwitterEngine.client.Do(req)
         
     if err != nil {
         return fmt.Errorf("Post request failed: %s", err)
@@ -455,7 +357,7 @@ func main() {
     flag.Parse()
     args := flag.Args()
     if len(args) < 3 {
-        fmt.Println("Please pass arguments: Email, Password, Comment. e.g. twitter.exe test@gmail.com password text to post")
+        fmt.Println("Please pass arguments: Email, Password, Comment. e.g. ./twitter test@gmail.com password this text will be tweeted")
         return
     }
 
@@ -464,10 +366,10 @@ func main() {
     var pass = args[1]
     var text = args[2:]
         
-    var fbengine FBEngine = *NewFBEngine()
+    var TwitterEngine TwitterEngine = *NewTwitterEngine()
  
-    authenticity_token, _ := twitter_login(&fbengine, email, pass)
-    twitter_geo_locate(&fbengine, authenticity_token)
-    logout_url, _ := twitter_post_comment(&fbengine, authenticity_token, text)
-    twitter_logout(&fbengine, authenticity_token, logout_url)
+    authenticity_token, _ := twitter_login(&TwitterEngine, email, pass)
+    twitter_geo_locate(&TwitterEngine, authenticity_token)
+    logout_url, _ := twitter_post_comment(&TwitterEngine, authenticity_token, text)
+    twitter_logout(&TwitterEngine, authenticity_token, logout_url)
 }	
