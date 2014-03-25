bbdd407650355fd5f11859c94c45a580fdeff529
geraldstanje <Gerald.Stanje@gmail.com>
2014-03-24 21:20:58 -0700

    updated multithreading code

--- encryptUtil/main.cpp
+++ encryptUtil/main.cpp
@@ -5,24 +5,24 @@ int main(int argc, char *argv[])
   // copy all arguments in a container of strings
   std::vector<std::string> allArgs(argv, argv + argc);
  
-  // argc should be 5 for a correct execution
-  if (allArgs.size() != 5) {
+	// argc should be 5 for a correct execution
+	if (allArgs.size() != 5) {
     // argv[0] is the program name
     std::cerr << "usage: "<< allArgs[0] << " [-n #] [-k keyfile]\n";
-    return -1;
-  }
+		return -1;
+	}
     
   EncryptUtil crypto;
   if(!crypto.read_key_from_file(allArgs[4])) {
-    return -1;
+  	return -1;
   }
   if(!crypto.set_number_of_threads(allArgs[2])) {
-    return -1;
+  	return -1;
   }
-  crypto.set_stream_buffer_size(10000);
+  crypto.set_stream_buffer_size(1000);
   if(!crypto.input_stream_encrypt()) {
-    return -1;
+  	return -1;
   }
   
-  return 0;
+	return 0;
 }
