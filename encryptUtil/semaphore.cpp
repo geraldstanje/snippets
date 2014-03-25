52c5adbc764ce1bcffada9a843a666626f816335
geraldstanje <Gerald.Stanje@gmail.com>
2014-03-24 21:44:07 -0700

    formatted code

--- encryptUtil/semaphore.cpp
+++ encryptUtil/semaphore.cpp
@@ -3,8 +3,8 @@
 semaphore::semaphore(): count(0) {}
        
 void semaphore::reset() {
-	std::unique_lock<std::mutex> lck(mtx);
- 	count = 0;
+  std::unique_lock<std::mutex> lck(mtx);
+  count = 0;
 }
     
 void semaphore::notify() {
@@ -14,6 +14,6 @@ void semaphore::notify() {
 }
     
 void semaphore::wait(unsigned int thread_id) {
-	std::unique_lock<std::mutex> lck(mtx);
-	cv.wait(lck, [this, &thread_id]() { return count == thread_id; });
+  std::unique_lock<std::mutex> lck(mtx);
+  cv.wait(lck, [this, &thread_id]() { return count == thread_id; });
 }
