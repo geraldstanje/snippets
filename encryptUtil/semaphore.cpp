825a0ce5859dbe527e35e51555d7fbdc0384bb52
geraldstanje <Gerald.Stanje@gmail.com>
2014-03-24 21:17:29 -0700

    added semaphore implementation

---
+++ encryptUtil/semaphore.cpp
@@ -0,0 +1,19 @@
+#include "semaphore.h"
+
+semaphore::semaphore(): count(0) {}
+       
+void semaphore::reset() {
+	std::unique_lock<std::mutex> lck(mtx);
+ 	count = 0;
+}
+    
+void semaphore::notify() {
+  std::unique_lock<std::mutex> lck(mtx);
+  ++count;
+  cv.notify_all();
+}
+    
+void semaphore::wait(unsigned int thread_id) {
+	std::unique_lock<std::mutex> lck(mtx);
+	cv.wait(lck, [this, &thread_id]() { return count == thread_id; });
+}
