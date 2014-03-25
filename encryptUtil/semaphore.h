3e9f72c378002af87a6068e883e8848aa2cfc400
geraldstanje <Gerald.Stanje@gmail.com>
2014-03-24 21:16:56 -0700

    added semaphore implementation

---
+++ encryptUtil/semaphore.h
@@ -0,0 +1,20 @@
+#ifndef semaphore_h
+#define semaphore_h
+
+#include <mutex>
+#include <condition_variable>
+
+class semaphore {
+private:
+    std::mutex mtx;
+    std::condition_variable cv;
+	unsigned int count;
+		
+public:
+    semaphore();
+    void reset();
+    void notify();
+    void wait(unsigned int thread_id);
+};
+
+#endif
